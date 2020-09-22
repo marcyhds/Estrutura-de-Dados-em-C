1273d8e12d549f784d43025a578396214854dcbc
Marcela Belchior <43523862+marcyhds@users.noreply.github.com>
2020-09-22 15:59:27 -0300

    Delete listaString.h

--- Prática 4/listaString/listaString.h
+++
@@ -1,7 +0,0 @@
-typedef struct lista * Lista;
-Lista inicializar_lista();
-int lista_vazia(Lista lst);
-int lista_cheia(Lista lst);
-int insere_elem(Lista lst, char elem[]);
-int remove_elem(Lista lst, char elem[]);
-void imprime(Lista lst);
